c27ec19b3f0e81ca272e26ffa63f4440be524aa6
Saputra1984 <syafutra84@gmail.com>
2026-07-20 17:31:52 +0700

    Enhance service worker activation and fetch handling
    Added skipWaiting and clients.claim to improve service worker activation and control.

--- sw.js
+++ sw.js
@@ -9,9 +9,20 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  self.skipWaiting(); // Memaksa service worker baru langsung aktif
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim()); // Mengambil alih kontrol halaman dengan cepat
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
